be0b86efe11ccd0760a2378046775487338aacc6
Thiago Mota <th.araujomota@gmail.com>
2022-02-09 19:00:58 -0300

    test: adds first test requirement

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,7 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verify if `fetchProduct` is a function', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
 });

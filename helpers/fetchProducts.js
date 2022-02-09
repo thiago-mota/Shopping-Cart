e321473d3c1f20d6e39552725e323570d924e5ed
Thiago Mota <th.araujomota@gmail.com>
2022-02-09 18:50:51 -0300

    feat: adds fetchProducts function

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const apiLink = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
+  const request = await fetch(apiLink);
+  const data = await request.json();
+
+  return data.results;
 };
+
+console.log(fetchProducts())
 
 if (typeof module !== 'undefined') {
   module.exports = {

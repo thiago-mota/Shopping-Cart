ebdc8baa26d84eda2fffebada5fc7b6d8256eb05
Thiago Mota <th.araujomota@gmail.com>
2022-02-10 18:56:05 -0300

    feat: adds fetchItem function

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemId) => {
+  const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+  const result = await fetch(endpoint);
+  const data = await result.json();
+
+  return data;
 };
+
+console.log(fetchItem('MLB1341706310'));
 
 if (typeof module !== 'undefined') {
   module.exports = {

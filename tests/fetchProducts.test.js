931fc6d666440bfe63d2be332dd8da502ea58d33
Thiago Mota <th.araujomota@gmail.com>
2022-02-11 00:55:00 -0300

    test: adds call fetch test

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,9 +1,15 @@
 require('../mocks/fetchSimulator');
+const stripFunctionStart = require('mochawesome/src/stripFnStart');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  it('Verify if `fetchProduct` is a function', () => {
+  it('verify if `fetchProduct` is a function', () => {
     expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('should call fetch', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled()
   });
 });

02b13d5c5f68b77f4cd986a989cdea2b8b01882e
Thiago Mota <th.araujomota@gmail.com>
2022-02-11 20:51:48 -0300

    feat: add test to check if fetchItem is a function

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,8 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('verify if `fetchItem` is a function', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
 });

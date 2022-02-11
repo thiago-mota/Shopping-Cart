959c0de48b3a0fc6e68736a637e21e8b668b1731
Thiago Mota <th.araujomota@gmail.com>
2022-02-11 20:49:40 -0300

    refac: cleaning fetchProducts function

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,6 @@ const fetchProducts = async (product) => {
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
   const result = await fetch(endpoint);
   const data = await result.json();
-
   return data.results;
 };
 

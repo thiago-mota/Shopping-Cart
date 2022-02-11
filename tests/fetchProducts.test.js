c0bb46c6888c7afdf38815962e8d672cf297ec82
Thiago Mota <th.araujomota@gmail.com>
2022-02-11 20:50:37 -0300

    refac: cleaning fetchProduct test

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -27,7 +27,7 @@ describe('1 - Teste a função fecthProducts', () => {
    try { 
      await fetchProducts();
    } catch (error) {
-   expect(error.message).toEqual(new Error('You must provide an url'));
+   expect(error).toEqual(new Error('You must provide an url'));
    }
   });
 });

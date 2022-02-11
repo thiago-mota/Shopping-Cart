771dc972e8a9b174a42c858e4d899e7d5f1d973e
Thiago Mota <th.araujomota@gmail.com>
2022-02-11 20:53:19 -0300

    test: check if its using correct endpoint

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -11,4 +11,9 @@ describe('2 - Teste a função fecthItem', () => {
     await fetchItem('MLB1615760527');
     expect(fetch).toBeCalled()
   });
+
+  it('verify if fetch function with `MLB1615760527` as argument uses the endpoint https://api.mercadolibre.com/items/MLB1615760527', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
 });

c5e48a751746c4a59484652d5722d8cefd91f615
Thiago Mota <th.araujomota@gmail.com>
2022-02-12 04:01:59 -0300

    feat: adds saveCartItems function

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,4 @@
-const saveCartItems = () => {
-  // seu código aqui
-};
+const saveCartItems = (key, value) => localStorage.setItem(key, value);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;

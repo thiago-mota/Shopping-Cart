db63a8ade9e14802afcdb82dade5ff3d1d24a35e
Thiago Mota <th.araujomota@gmail.com>
2022-02-12 16:33:48 -0300

    test: adds 2nd test on saveCartItems function

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,4 +8,10 @@ describe('4 - Teste a função saveCartItems', () => {
     saveCartItems('<ol><li>Item</li></ol>')
     expect(localStorage.setItem).toBeCalled();
   });
+
+
+  it('test if when running saveCartItems function with `<ol><li>Item</li></ol>` as argument, the method localStorage.setItem is called with two parameters: 1st `cartItems` and 2nd being the value used as argument on saveCartItems' , () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });

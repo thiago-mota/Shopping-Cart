22a77a71faeaf7552156ae83efce737f618f7506
Thiago Mota <th.araujomota@gmail.com>
2022-02-12 15:41:50 -0300

    refac: changed parameters on saveCartItems function

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,4 +1,4 @@
-const saveCartItems = (key, value) => localStorage.setItem(key, value);
+const saveCartItems = (value) => localStorage.setItem('cartItems', value);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;

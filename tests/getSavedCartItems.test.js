dc6a29c69821cfc2b4da05c7df3c47c35a839dea
Thiago Mota <th.araujomota@gmail.com>
2022-02-12 15:22:52 -0300

    test: adds 1st getSavedCartItems test

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,8 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('check if when running getSavedCartItems function the localStorage.getItem method  is called', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
 });

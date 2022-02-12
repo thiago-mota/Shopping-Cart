9dd95d0e27cc7ab26c2cb9a4c949c77618445a45
Thiago Mota <th.araujomota@gmail.com>
2022-02-12 15:23:11 -0300

    test: adds 2nd getSavedCartItems test

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -8,4 +8,9 @@ describe('4 - Teste a função getSavedCartItems', () => {
     getSavedCartItems();
     expect(localStorage.getItem).toBeCalled();
   });
+
+  it('check if when running getSavedCartItems function the localStorage.getItem method is called using `cartItems` as parameter', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  });
 });

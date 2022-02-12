c324f59175ed142d9844c5709dcc4c75e64b62ba
Thiago Mota <th.araujomota@gmail.com>
2022-02-12 15:34:15 -0300

    test: adds 1st saveCartItems test

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,8 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('test if locallStorage.setItem is called when running saveCartItems function using `<ol><li>Item</li></ol>` as arguement', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toBeCalled();
+  });
 });
